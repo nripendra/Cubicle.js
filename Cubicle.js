4fa567abd7eaea0ada8d6ae18fa9d1f1477c3149
nripendra <nripendra@uba-solutions.com>
2014-12-09 07:57:59 +0545

    Changed tabsettings

--- Cubicle.js
+++ Cubicle.js
@@ -1,96 +1,94 @@
 ﻿(function (global) {
-	var cubicles = {};
+    var cubicles = {};
 
-	var isFunction = function (variable) {
-		return (typeof variable === "function");
-	};
+    var isFunction = function (variable) {
+        return (typeof variable === "function");
+    };
 
-	var exportCubicle = function (cubicleName, cubicle, reigsterToGlobal) {
+    var exportCubicle = function (cubicleName, cubicle, reigsterToGlobal) {
 
-		var namespaceParts = cubicleName.split('.');
+        var namespaceParts = cubicleName.split('.'),
+            rootObject = (reigsterToGlobal === true) ? global : cubicles;
 
-		var rootObject = (reigsterToGlobal === true) ? global : cubicles;
+        for (var j = 0; j < namespaceParts.length - 1; j++) {
+            var currentPart = namespaceParts[j],
+                ns = rootObject[currentPart];
+            if (!ns) {
+                ns = rootObject[currentPart] = {};
+            }
+            rootObject = ns;
+        }
 
-		for (var j = 0; j < namespaceParts.length - 1; j++) {
-			var currentPart = namespaceParts[j];
+        rootObject[namespaceParts[namespaceParts.length - 1]] = cubicle;
 
-			var ns = rootObject[currentPart];
-			if (!ns) {
-				ns = rootObject[currentPart] = {};
-			}
-			rootObject = ns;
-		}
+        return cubicle;
+    };
 
-		rootObject[namespaceParts[namespaceParts.length - 1]] = cubicle;
+    var importCubicle = function (cubicleName) {
+        var namespaceParts = cubicleName.split('.'),
+            rootObject = cubicles;
 
-		return cubicle;
-	};
+        for (var j = 0; j < namespaceParts.length; j++) {
+            var currentPart = namespaceParts[j],
+                ns = rootObject[currentPart];
 
-	var importCubicle = function (cubicleName) {
-		var namespaceParts = cubicleName.split('.');
-		var rootObject = cubicles;
+            rootObject = ns;
 
-		for (var j = 0; j < namespaceParts.length; j++) {
-			var currentPart = namespaceParts[j];
-
-			var ns = rootObject[currentPart];
-
-			rootObject = ns;
-
-			if (!ns) {
-				break;
-			}
-		}
-		return rootObject;
-	};
+            if (!ns) {
+                break;
+            }
+        }
+        return rootObject;
+    };
 
 	//#nrip - "Cubicle" still clutters the global namespace :P
-	global["Cubicle"] = function () {
-		var args = arguments;
-		var len = args.length;
-		var i = 0;
+    global["Cubicle"] = function () {
+        var args = arguments,
+            len = args.length,
+            i = 0;
 
-		for (i = 0; i < len; i++) {
-			var callback = args[i];
-			if (isFunction(callback)) {
-				var f = function () { }
-				var cubicle = callback.apply(global, [importCubicle, exportCubicle]);
-				if (cubicle) {
-					if (cubicle.init && isFunction(cubicle.init)) {
-						cubicle.init();
-					}
-				}
-			}
-		}
-	};
-	
-	Cubicle(function(Import, Export){
-		return Export("messaging", {
-			init: function () {
-				this._list = [];
-			},
-			listen: function(channel, obj, callback) {
-				this._list.push({"channel": channel, "context": obj, "callback": callback});
-			},
-			unlisten: function(channel, obj ) {
-				for( var i = 0, len = this._list.length; i < len; i++ ) {
-					var listener = this._list[i];
-					if(listener.channel === channel && listener.context === obj){
-						this._list.splice( i, 1 );
-						return true;
-					}
-				}
-				return false;
-			},
-			publish: function(channel, message) {
-				for( var i = 0, len = this._list.length; i < len; i++ ) {
-					var listener = this._list[i];
-					if(listener.channel === channel){
-						listener.callback.apply(listener.context, [message]);
-					}
-				}
-			}
-		});
-	});	
+        for (i = 0; i < len; i++) {
+            var callback = args[i];
+            if (isFunction(callback)) {
+                var f = function () { },
+                    cubicle = callback.apply(global, [importCubicle, exportCubicle]);
+                    
+                if (cubicle) {
+                    if (cubicle.init && isFunction(cubicle.init)) {
+                        cubicle.init();
+                    }
+                }
+            }
+        }
+    };
+
+    Cubicle(function(Import, Export){
+        return Export("messaging", {
+            init: function () {
+                this._list = [];
+            },
+            listen: function(channel, obj, callback) {
+                this._list.push({"channel": channel, "context": obj, "callback": callback});
+            },
+            unlisten: function(channel, obj ) {
+                for( var i = 0, len = this._list.length; i < len; i++ ) {
+                    var listener = this._list[i];
+                    if(listener.channel === channel && listener.context === obj){
+                        this._list.splice( i, 1 );
+                        return true;
+                    }
+                }
+                return false;
+            },
+            publish: function(channel, message) {
+                for( var i = 0, len = this._list.length; i < len; i++ ) {
+                    var listener = this._list[i];
+                    if(listener.channel === channel){
+                        listener.callback.apply(listener.context, [message]);
+                    }
+                }
+            }
+        });
+    });	
 })(this);
 
